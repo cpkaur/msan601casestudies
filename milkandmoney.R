f903e2e78006460a6bfcaad30bf9c3a55dab4854
Chaman Preet Kaur <ckaur3@dons.usfca.edu>
2015-10-04 11:10:48 -0700

    Testing more models
    Added ramsay test and tested for powers 2 and 3. Testing more models on
    tis basis

--- milkandmoney.R
+++ milkandmoney.R
@@ -11,7 +11,7 @@ summary(model)
 
 model_1 <- lm(Mailbox ~ Class.III , data = milk[2:n])
 summary(model_1)
-model_2 <- lm(Mailbox ~ (Class.IV) , data = milk[2:n])
+model_2 <- lm(Mailbox ~ Class.IV , data = milk[2:n])
 summary(model_2)
 model_2t <- lm(Mailbox ~ log(Class.IV) , data = milk[2:n])
 summary(model_2t)
@@ -25,5 +25,68 @@ summary(model_5)
 
 pairs(milk[2:n])
 
+library(lmtest)
+resettest(model , power=2, type="regressor")
+resettest(model , power=3, type="regressor")
 
+model_6 <- lm(milk$Mailbox ~ milk$Class.IV + milk$Class.III +
+                milk$Butter + milk$NFDM + I(milk$Class.IV^2))
+summary(model_6)
 
+model_7 <- lm(milk$Mailbox ~ milk$Class.IV + milk$Class.III +
+                milk$Butter + milk$NFDM + I(milk$Class.III^2))
+summary(model_7)
+
+model_8 <- lm(milk$Mailbox ~ milk$Class.IV + milk$Class.III +
+                milk$Butter + milk$NFDM + I(milk$Butter^2))
+summary(model_8)
+
+model_9 <- lm(milk$Mailbox ~ milk$Class.IV + milk$Class.III +
+                milk$Butter + milk$NFDM + I(milk$NFDM^2))
+summary(model_9)
+
+model_10 <- lm(milk$Mailbox ~ milk$Class.IV + milk$Class.III +
+                milk$Butter + milk$NFDM + 
+                 milk$Class.IV*milk$Class.III*milk$Butter*milk$NFDM)
+summary(model_10)
+
+model_11 <- lm(milk$Mailbox ~ milk$Class.IV + milk$Class.III + 
+                 milk$Class.IV*milk$Class.III*milk$Butter*milk$NFDM)
+summary(model_11)
+
+model_12 <- lm(milk$Mailbox ~ milk$Class.IV + milk$Class.III + 
+                 milk$Class.IV:milk$Class.III:milk$Butter:milk$NFDM)
+summary(model_12)
+
+model_13 <- lm(milk$Mailbox ~ milk$Class.IV + milk$Class.III + 
+                 milk$Class.IV:milk$Class.III)
+summary(model_13)
+
+model_14 <- lm(milk$Mailbox ~ milk$Class.IV + milk$Class.III + 
+                 milk$Butter:milk$NFDM)
+summary(model_14)
+
+model_15 <- lm(milk$Mailbox ~ milk$Class.IV + milk$Class.III + 
+                 milk$Class.IV:milk$Class.III:milk$Butter)
+summary(model_15)
+
+model_16 <- lm(milk$Mailbox ~ milk$Class.IV + milk$Class.III + 
+                 milk$Class.IV:milk$Class.III + 
+                 milk$Class.IV:milk$Class.III:milk$Butter)
+summary(model_16)
+
+model_17 <- lm(milk$Mailbox ~ milk$Class.IV + milk$Class.III + 
+                 milk$Class.IV:milk$Class.III + 
+                 milk$Class.IV:milk$Class.III:milk$NFDM)
+summary(model_17)
+
+model_18 <- lm(milk$Mailbox ~ milk$Class.IV + milk$Class.III + 
+                 milk$Butter:milk$Class.III)
+summary(model_18)
+
+cor(milk[3:6])
+
+model_19 <- lm(milk$Mailbox ~ milk$Class.IV + milk$Class.III + 
+                 milk$Butter:milk$Class.III +
+                 milk$Butter:milk$Class.IV)
+summary(model_19)
