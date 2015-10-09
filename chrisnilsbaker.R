80dd4f9fafde163dfaf1899f571a929c992d702d
catterbu <cbatterbury@dons.usfca.edu>
2015-10-09 13:54:28 -0700

    Explore data set

--- chrisnilsbaker.R
+++ chrisnilsbaker.R
@@ -1,6 +1,30 @@
 d <- read.csv("41330723.csv", header = TRUE, stringsAsFactors = FALSE)
+d <- d[1:120, ] # last two rows contain no data
+names(d) <- c("ID", "Total.Households", "Accounts", "Footprint")
 View(d)
-head(d[["Inside.Outside.Footprint"]])
-head(d[["Inside.Outside.Footprint"]][d[["Inside.Outside.Footprint"]] == "Outside"])
+for (i in 2:3) {
+  d[[i]] <- gsub(",", "", d[[i]])
+  d[[i]] <- as.numeric(d[[i]])
+}
+d[["Footprint"]][d[["Footprint"]] == "Outside"] <- 0
+d[["Footprint"]][d[["Footprint"]] == "Inside"] <- 1
+head(d[["Footprint"]])
+d[["Footprint"]] <- as.numeric(d[["Footprint"]])
+
+# SLR models
+lm_1 <- lm(Accounts ~ Total.Households, data = d)
+summary(lm_1)
+
+lm_2 <- lm(Accounts ~ Footprint, data = d)
+summary(lm_2)
+
+# MLR Models
+lm_3 <- lm(Accounts ~ Total.Households + Footprint, data = d)
+summary(lm_3)
+
+lm_4 <- lm(Accounts ~ Total.Households + Footprint +
+             Total.Households:Footprint, data = d)
+summary(lm_4)
+
 
 
