3700783feb39830de36e4fe68cf5dbe27e5350ea
catterbu <cbatterbury@dons.usfca.edu>
2015-10-15 15:52:38 -0700

    Add libraries at top

--- chrisnilsbaker.R
+++ chrisnilsbaker.R
@@ -1,3 +1,6 @@
+library(car)
+library(MASS)
+
 d <- read.csv("41330723.csv", header = TRUE, stringsAsFactors = FALSE)
 d <- d[1:120, ] # last two rows contain no data
 names(d) <- c("ID", "Total.Households", "Accounts", "Footprint")
@@ -10,6 +13,8 @@ d[["Footprint"]][d[["Footprint"]] == "Outside"] <- 0
 d[["Footprint"]][d[["Footprint"]] == "Inside"] <- 1
 head(d[["Footprint"]])
 d[["Footprint"]] <- as.numeric(d[["Footprint"]])
+d[["ID"]] <- as.numeric(d[["ID"]])
+cor(d)
 
 # SLR models
 lm_1 <- lm(Accounts ~ Total.Households, data = d)
@@ -23,7 +28,7 @@ lm_3 <- lm(Accounts ~ Total.Households + Footprint, data = d)
 summary(lm_3)
 
 lm_4 <- lm(Accounts ~ Total.Households + Footprint +
-             Total.Households:Footprint, data = d)
+           Total.Households:Footprint, data = d)
 summary(lm_4)
 
 
